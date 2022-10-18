18489d94c05ca72ca0d5e5c58bd51e6af5090e0f
kennankole <kennankole@gmail.com>
2022-10-18 13:55:24 +0300

    Created and styled the home page UI

--- src/index.js
+++ src/index.js
@@ -1,10 +1,29 @@
 import _ from 'lodash';
 import './style.css';
+import dataList from './modules/home.js'
 
-function component() {
-    const element = document.createElement('div');
-    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-    return element;
-}
 
-document.body.appendChild(component());
+const data = dataList();
+let divElement = document.createElement("div");
+let refreshBtn = document.createElement('div');
+const scores = document.getElementById('scores-container');
+const olElements = document.createElement('ul');
+
+
+divElement.classList.add('elem-container');
+refreshBtn = `
+    <div class="refresh-btn">
+        <h3>Recent scores</h3>
+        <button type="submit" id="reload-btn">Refresh</button>
+    </div>
+`;
+
+
+olElements.classList.add('ul-elements')
+
+data.forEach((item) => {
+    olElements.innerHTML += `<li class="players-li">Name ${item}</li>`;
+});
+divElement.appendChild(olElements);
+
+scores.appendChild(divElement);
